6c92c5ededd5b82cf6a0a8cf90fb198a190ae042
phamhung99 <phamhung99.ltv@gmail.com>
2018-10-09 01:40:31 +0700

    Add new file

---
+++ DicCommandline/src/dictionary/Dictionary.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author phamh
+ */
+public class Dictionary {
+    static ArrayList<Word> listWord = new ArrayList<>();
+    
+}
